d25acfea9abb58a1ad85efa5080565d06507a878
Vainer03 <avainer03@gmail.com>
2022-06-12 15:46:27 +0000

    add header for client-write & mod client-write

--- client/src/clientw.c
+++ client/src/clientw.c
@@ -5,14 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
+
+#include <errno.h>
 #include <netdb.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
+
+#include "clientw.h"
 
 #define PORT "8765" // the port client will be connecting to 
 
