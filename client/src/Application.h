4c3c5449e3303caefba3b6c123e36014ea1196af
Archie <archie-woc@ya.ru>
2022-06-15 06:12:08 +0300

    Add ui implementation

--- client/src/Application.h
+++ client/src/Application.h
@@ -1,9 +1,37 @@
 #pragma once
+#include "ApplicationState.h"
+#include "MessageList.h"
+#include "RenderData.h"
+
 #include <stdbool.h>
+#include <nuklear_cross.h>
+
+#define WINDOW_HEIGHT   480
+#define WINDOW_WIDTH    640
+#define MAX_ROOM_NUMBER_SIZE 4
+#define MAX_NICKNAME_SIZE 16
 
 typedef struct {
+    /* Nuklear specific data and render data */
+    struct nkc* nkc_handle;
+    struct nk_context* ctx;
+    RenderData* render_data;
+
+    /* Window data */
     bool is_running;
+    int window_height;
+    int window_width;
+
+    /* Logic data */
+    MessageList* message_list;
+    ApplicationState application_state;
+    char room_number[MAX_ROOM_NUMBER_SIZE];
+    int room_number_size;
+    char nickname[MAX_NICKNAME_SIZE];
+    int nickname_size;
 } Application;
 
 Application* create_application();
 void delete_application(Application* app);
+void update(Application* app);
+void render(Application* app);
---
+++ client/src/RenderData.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "MessageList.h"
+
+typedef struct {
+    char box_buffer[MAX_MESSAGE_LIST_COUNT*MAX_MESSAGE_SIZE];
+    int box_buffer_len;
+    char line_text[MAX_MESSAGE_SIZE];
+    int line_text_len;
+} RenderData;
+
+RenderData* create_render_data();
+void delete_render_data(RenderData* rd);
